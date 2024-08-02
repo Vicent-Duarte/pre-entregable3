7a9f346739ea18f9b89188fa86d054b7ce6dea13
Sebastian Casallas <sebastiancasallasarias2020@gmail.com>
2024-08-01 22:14:17 -0500

    Genre Fixed

--- src/controllers/genre.controllers.js
+++ src/controllers/genre.controllers.js
@@ -2,16 +2,43 @@ const catchError = require('../utils/catchError');
 const Genre = require('../models/Genre.model');
 
 const getAll = catchError(async(req, res) => {
-    const getAll = await Genre.findAll()
-    return res.json(getAll)
+    const results = await Genre.findAll();
+    return res.json(results);
 });
 
-const createGenre = catchError(async(req, res) => {
-    const createGenre = await Genre.create(req.body)
-    return res.json(createGenre)
-})
+const create = catchError(async(req, res) => {
+    const result = await Genre.create(req.body);
+    return res.status(201).json(result);
+});
+
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params;
+    const result = await Genre.findByPk(id);
+    if(!result) return res.sendStatus(404);
+    return res.json(result);
+});
+
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    const result = await Genre.destroy({ where: {id} });
+    if(!result) return res.sendStatus(404);
+    return res.sendStatus(204);
+});
+
+const update = catchError(async(req, res) => {
+    const { id } = req.params;
+    const result = await Genre.update(
+        req.body,
+        { where: {id}, returning: true }
+    );
+    if(result[0] === 0) return res.sendStatus(404);
+    return res.json(result[1][0]);
+});
 
 module.exports = {
     getAll,
-    createGenre
+    create,
+    getOne,
+    remove,
+    update
 }
