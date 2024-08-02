02a04ebdd9c71629cb5b4dbd5aa3b111419604f1
williamdrg <123426425+williamdrg@users.noreply.github.com>
2024-08-01 22:25:33 -0500

    rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const routerArtists = require('./artis.outer');
+const routerAlbum = require('./albums.router');
+const routerSong = require('./songs.router');
+const routerGenre = require('./genre.router');
 const router = express.Router();
 
 router.use('/artists', routerArtists)
-
+router.use('/albums', routerAlbum)
+router.use('/song', routerSong)
+router.use('/song', routerGenre)
 
 module.exports = router;
